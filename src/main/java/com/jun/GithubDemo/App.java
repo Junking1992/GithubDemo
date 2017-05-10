ac85ea284b0269205826c27ba6fdd1c579fd1ed1
know <wangjun@whknow.cn>
2017-05-10 23:40:45 +0800

    Signed-off-by: know <wangjun@whknow.cn>

--- src/main/java/com/jun/GithubDemo/App.java
+++ src/main/java/com/jun/GithubDemo/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!zz1993" );
+        System.out.println( "Hello World!Junking1992" );
     }
 }
